add9d7b16d94d1c240a45a8e6353ddab16099439
Titux Metal (DEV) <github@lgdweb.fr>
2023-12-20 06:30:15 +0100

    Add welcome message and icon to index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,4 @@
+import { Bars3Icon } from '@heroicons/react/24/outline'
 import { type MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
@@ -8,6 +9,12 @@ export default function Index() {
   return (
     <div className='container m-auto flex min-h-screen flex-col space-y-8 border-2 p-4'>
       <h1 className='text-6xl font-bold text-accent underline'>Hello world!</h1>
+      <p className='text-2xl'>Welcome to your new Remix app.</p>
+      <p className='text-2xl'>
+        Lets go for a{' '}
+        <Bars3Icon className='inline-block h-8 w-8 cursor-pointer rounded-full border border-success p-1 text-green-400 hover:border-accent hover:text-accent' />{' '}
+        !
+      </p>
       <section className='flex gap-6'>
         <button className='btn'>Button</button>
         <button className='btn btn-neutral'>Neutral</button>
